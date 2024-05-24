4a568438744417762aa3126e63438a1981d006cc
Harshk3610 <117203974+Harshk3610@users.noreply.github.com>
2024-05-24 09:04:54 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -22,7 +22,7 @@ if st.button('Use Camera'):
     frameST = st.empty()
 
     # Start webcam
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(1)
 
     while True:
         ret, frame = cap.read()
